b4053e708a282b151cbefa45d97a4aa1fc31b9d0
Ziloka <50429450+ziloka@users.noreply.github.com>
2022-08-13 06:19:08 -0400

    impl keyboard input

--- src/consts.rs
+++ src/consts.rs
@@ -1,21 +1,38 @@
 use bevy::prelude::*;
 
-pub const BOARD_HEIGHT: f32 = 20.0; // 20 squares tall
-pub const BOARD_WIDTH: f32 = 10.0; // 10 squares wide
-pub const TETRIOMINO_SIDE_LENGTH: f32 = 40.0;
+// change handling (measured in frames per movement)
+pub const ARR: f32 = 1.6;
+pub const DAS: f32 = 8.3;
+pub const DCD: f32 = 1.0;
+pub const SDF: f32 = std::f32::INFINITY;
 
-pub struct bottom_left_corner {
-    pub X: f32,
-    pub Y: f32,
+struct KEYS;
+
+impl KEYS {
+    pub const CLOCKWISE: KeyCode = KeyCode::Up;
+    pub const COUNTER_CLOCKWISE: KeyCode = KeyCode::Z;
+    pub const MOVE_LEFT: KeyCode = KeyCode::Left;
+    pub const MOVE_RIGHT: KeyCode = KeyCode::Right;
+    pub const HARDDROP_KEY: KeyCode = KeyCode::Space;
 }
 
-pub const BOTTOM_LEFT_CORNER: bottom_left_corner = bottom_left_corner {
-    X: (BOARD_WIDTH / 2.0) * TETRIOMINO_SIDE_LENGTH,
-    Y: (BOARD_HEIGHT / 2.0) * TETRIOMINO_SIDE_LENGTH
-};
+pub struct Board {}
+
+impl Board {
+    pub const HEIGHT: f32 = 20.0; // 20 squares tall
+    pub const WIDTH: f32 = 10.0; // 10 squares wide
+    pub const TETRIOMINO_SIDE_LENGTH: f32 = 40.0; // 40 pixels per side (square)
+}
+
+pub struct TopRightCorner {}
+
+impl TopRightCorner {
+    pub const X: f32 = (Board::WIDTH / 2.0) * Board::TETRIOMINO_SIDE_LENGTH;
+    pub const Y: f32 = (Board::HEIGHT / 2.0) * Board::TETRIOMINO_SIDE_LENGTH;
+}
 
 // "different data types (Components) are like the "columns" of a table, and there can be arbitrarily many "rows" (Entities) containing values / instances of various components."
-#[derive(Component)]
+#[derive(Component, Clone)]
 pub enum TetriminoType {
     I,
     O,
@@ -39,3 +56,13 @@ impl TetriminoType {
         }
     }
 }
+
+pub const TETRIMINO_TYPES: [TetriminoType; 7] = [
+    TetriminoType::I,
+    TetriminoType::O,
+    TetriminoType::T,
+    TetriminoType::S,
+    TetriminoType::Z,
+    TetriminoType::S,
+    TetriminoType::J,
+];
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, input::{keyboard::KeyboardInput, ButtonState}};
 use rand::seq::SliceRandom;
 
 mod consts;
-use consts::{
-    TetriminoType, BOARD_HEIGHT, BOARD_WIDTH, BOTTOM_LEFT_CORNER, TETRIOMINO_SIDE_LENGTH,
-};
+use consts::{Board, TetriminoType, TopRightCorner, TETRIMINO_TYPES};
 
 fn main() {
     App::new()
@@ -12,12 +10,17 @@ fn main() {
         .insert_resource(ClearColor(Color::BLACK))
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_startup_system(selected_tetrimino)
+        .add_system(selected_tetrimino_movement_system)
         .run();
 }
 
-#[derive(Bundle)]
-struct TetriminoBundle {
+#[derive(Component)]
+struct nothing {
+    iamnothing: bool,
+}
+
+#[derive(Component)]
+struct SelectedTetrimino {
     tetrimino_type: TetriminoType,
 }
 
@@ -34,68 +37,71 @@ fn setup(
     commands.spawn_bundle(Camera2dBundle::default());
 
     // https://stackoverflow.com/questions/48490049/how-do-i-choose-a-random-value-from-an-enum
-    let types = [
-        TetriminoType::I,
-        TetriminoType::O,
-        TetriminoType::T,
-        TetriminoType::S,
-        TetriminoType::Z,
-        TetriminoType::S,
-        TetriminoType::J,
-    ];
+    let starting_piece = TETRIMINO_TYPES.choose(&mut rand::thread_rng()).unwrap();
 
-    println!("bottom left corner: x = {}, y = {}", BOTTOM_LEFT_CORNER.X, BOTTOM_LEFT_CORNER.Y);
-    // Spawn rectangles
-    for i in 1..=BOARD_WIDTH as u8 {
-        for j in 1..=BOARD_HEIGHT as u8 {
-            if (i as f32 == BOARD_WIDTH && j as f32 == BOARD_HEIGHT) || (i == 0 && j == 0) {
-                println!("x = {}, y = {}", (i as f32 * TETRIOMINO_SIDE_LENGTH) - BOTTOM_LEFT_CORNER.X, (j as f32 * TETRIOMINO_SIDE_LENGTH) - BOTTOM_LEFT_CORNER.Y)
-            }
-            commands.spawn_bundle(MaterialMesh2dBundle {
-                mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
-                transform: Transform {
-                    translation: Vec3 {
-                        x: (i as f32 * TETRIOMINO_SIDE_LENGTH) - BOTTOM_LEFT_CORNER.X,
-                        y: (j as f32 * TETRIOMINO_SIDE_LENGTH) - BOTTOM_LEFT_CORNER.Y,
-                        z: 0.0,
-                    },
-                    rotation: Quat::default(),
-                    scale: Vec3::splat(40.0),
+    commands
+        .spawn()
+        .insert(SelectedTetrimino {
+            tetrimino_type: starting_piece.clone(),
+        })
+        .insert_bundle(MaterialMesh2dBundle {
+            mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+            // If you are overriding the camera transform / creating your own transform, you need to do this! The default transform (with Z=0.0) will place the camera so that your sprites (at positive +Z coordinates) would be behind the camera, and you wouldn't see them!
+            // https://bevy-cheatbook.github.io/pitfalls/2d-camera-z.html
+            transform: Transform {
+                translation: Vec3 {
+                    x: TopRightCorner::X - (Board::WIDTH / 2.0 * Board::TETRIOMINO_SIDE_LENGTH),
+                    y: TopRightCorner::Y,
+                    z: 2.0,
                 },
-                // material: materials.add(ColorMaterial::from(Color::GRAY)),
-                material: materials.add(ColorMaterial::from(types.choose(&mut rand::thread_rng()).unwrap().get_color())),
-                ..default()
-            });
+                rotation: Quat::default(),
+                scale: Vec3::splat(Board::TETRIOMINO_SIDE_LENGTH),
+            },
+            material: materials.add(ColorMaterial::from(starting_piece.get_color())),
+            ..default()
+        });
+
+    // Spawn rectangles
+    for i in 1..=Board::WIDTH as u8 {
+        for j in 1..=Board::HEIGHT as u8 {
+            // let tetrimino_type = TETRIMINO_TYPES;
+            commands
+                .spawn_bundle(MaterialMesh2dBundle {
+                    mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+                    // If you are overriding the camera transform / creating your own transform, you need to do this! The default transform (with Z=0.0) will place the camera so that your sprites (at positive +Z coordinates) would be behind the camera, and you wouldn't see them!
+                    // https://bevy-cheatbook.github.io/pitfalls/2d-camera-z.html
+                    transform: Transform {
+                        translation: Vec3 {
+                            x: (i as f32 * Board::TETRIOMINO_SIDE_LENGTH) - TopRightCorner::X,
+                            y: (j as f32 * Board::TETRIOMINO_SIDE_LENGTH) - TopRightCorner::Y,
+                            z: 1.0,
+                        },
+                        rotation: Quat::default(),
+                        scale: Vec3::splat(Board::TETRIOMINO_SIDE_LENGTH),
+                    },
+                    material: materials.add(ColorMaterial::from(Color::GRAY)),
+                    ..default()
+                })
+                .insert(nothing { iamnothing: true });
         }
     }
 }
 
-#[derive(Bundle)]
-struct SelectedTetriminoBundle {
-    tetriminoType: TetriminoType,
-}
-
-fn selected_tetrimino(
+// https://bevy-cheatbook.github.io/input/keyboard.html
+fn selected_tetrimino_movement_system(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut key_evr: EventReader<KeyboardInput>,
+    mut query: Query<(&SelectedTetrimino)>,
 ) {
-    // println!("x = {}, y = {}", 0.0, (BOTTOM_LEFT_CORNER.Y * BOARD_HEIGHT) * TETRIOMINO_SIDE_LENGTH);
-    // right now spawn a black dot in the middle of the "board"
-    commands.spawn_bundle(MaterialMesh2dBundle {
-        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
-        transform: Transform {
-            translation: Vec3 {
-                x: BOTTOM_LEFT_CORNER.Y,
-                y: BOTTOM_LEFT_CORNER.X,
-                // y: (BOTTOM_LEFT_CORNER.Y * BOARD_HEIGHT) * TETRIOMINO_SIDE_LENGTH,
-                z: 1.0,
-            },
-            rotation: Quat::default(),
-            scale: Vec3::splat(40.0),
-        },
-        material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        ..default()
-    });
+    let mut selected_tetrimino_transform = query.single_mut();
+    for ev in key_evr.iter() {
+        match ev.state {
+            ButtonState::Pressed => {
+                println!("Key press: {:?} ({})", ev.key_code, ev.scan_code);
+            }
+            ButtonState::Released => {
+                println!("Key release: {:?} ({})", ev.key_code, ev.scan_code);
+            }
+        }
+    }
 }
