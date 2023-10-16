5604ebe17655d28b34ad4a7fe39415c90f29e955
Ziloka <50429450+ziloka@users.noreply.github.com>
2023-10-15 20:12:51 -0400

    update github workflow

--- web.sh
+++ web.sh
@@ -35,7 +35,7 @@ echo "<html lang=\"en\">
     <canvas id=\"glcanvas\" tabindex='1' oncontextmenu="return false"></canvas>
     <!-- Minified and statically hosted version of https://github.com/not-fl3/macroquad/blob/master/js/mq_js_bundle.js -->
     <script src=\"https://not-fl3.github.io/miniquad-samples/mq_js_bundle.js\"></script>
-    <script>load(\"tetris.wasm\");</script> <!-- Your compiled wasm file -->
+    <script>load(\"client.wasm\");</script> <!-- Your compiled wasm file -->
 </body>
 </html>" > index.html
 
